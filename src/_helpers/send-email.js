1b34a87d84e024b2a6212d8634f8eb6515df280b
Kervy Tabigue <kervytabigue69@gmail.com>
2026-05-11 20:06:44 +0800

    Updated Files With frontend

--- src/_helpers/send-email.js
+++ src/_helpers/send-email.js
@@ -5,7 +5,8 @@ module.exports = sendEmail;
 async function sendEmail({ to, subject, html, from }) {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
+        port: parseInt(process.env.SMTP_PORT),
+        secure: process.env.SMTP_PORT == 465,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
