5c597eb7e338d5f84186995ab007fd47632adef7
unknown <YY137@ccs.local>
2026-05-13 11:21:12 +0800

    Updated Files

--- src/_helpers/send-email.js
+++ src/_helpers/send-email.js
@@ -1,19 +1,42 @@
-const SibApiV3Sdk = require('@getbrevo/brevo');
+const nodemailer = require('nodemailer');
+const https = require('https');
 
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html, from }) {
-    const defaultClient = SibApiV3Sdk.ApiClient.instance;
-    const apiKey = defaultClient.authentications['api-key'];
-    apiKey.apiKey = process.env.BREVO_API_KEY;
+    const data = JSON.stringify({
+        sender: { email: from || process.env.EMAIL_FROM },
+        to: [{ email: to }],
+        subject: subject,
+        htmlContent: html
+    });
 
-    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+    return new Promise((resolve, reject) => {
+        const options = {
+            hostname: 'api.brevo.com',
+            path: '/v3/smtp/email',
+            method: 'POST',
+            headers: {
+                'accept': 'application/json',
+                'content-type': 'application/json',
+                'api-key': process.env.BREVO_API_KEY
+            }
+        };
 
-    sendSmtpEmail.to = [{ email: to }];
-    sendSmtpEmail.sender = { email: from || process.env.EMAIL_FROM };
-    sendSmtpEmail.subject = subject;
-    sendSmtpEmail.htmlContent = html;
+        const req = https.request(options, (res) => {
+            let body = '';
+            res.on('data', (chunk) => body += chunk);
+            res.on('end', () => {
+                if (res.statusCode >= 200 && res.statusCode < 300) {
+                    resolve(body);
+                } else {
+                    reject(new Error(`Brevo API error: ${body}`));
+                }
+            });
+        });
 
-    await apiInstance.sendTransacEmail(sendSmtpEmail);
+        req.on('error', reject);
+        req.write(data);
+        req.end();
+    });
 }
