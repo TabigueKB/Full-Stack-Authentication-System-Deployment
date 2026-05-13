2f0fa46ef74c07633ea111230245a90420e44dd6
unknown <YY137@ccs.local>
2026-05-13 11:17:53 +0800

    Updated Files

--- src/_helpers/send-email.js
+++ src/_helpers/send-email.js
@@ -1,12 +1,15 @@
-const Brevo = require('@getbrevo/brevo');
+const SibApiV3Sdk = require('@getbrevo/brevo');
 
 module.exports = sendEmail;
 
 async function sendEmail({ to, subject, html, from }) {
-    const apiInstance = new Brevo.TransactionalEmailsApi();
-    apiInstance.authentications['apiKey'].apiKey = process.env.BREVO_API_KEY;
+    const defaultClient = SibApiV3Sdk.ApiClient.instance;
+    const apiKey = defaultClient.authentications['api-key'];
+    apiKey.apiKey = process.env.BREVO_API_KEY;
 
-    const sendSmtpEmail = new Brevo.SendSmtpEmail();
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
     sendSmtpEmail.to = [{ email: to }];
     sendSmtpEmail.sender = { email: from || process.env.EMAIL_FROM };
     sendSmtpEmail.subject = subject;
