0548341683dea7b58eecf6b2ae2722c2a6bd74d4
unknown <YY137@ccs.local>
2026-05-13 09:55:15 +0800

    Change SMTP service

--- src/_helpers/send-email.js
+++ src/_helpers/send-email.js
@@ -6,7 +6,7 @@ async function sendEmail({ to, subject, html, from }) {
     const transporter = nodemailer.createTransport({
         host: process.env.SMTP_HOST,
         port: parseInt(process.env.SMTP_PORT),
-        secure: process.env.SMTP_PORT == 465,
+        secure: false,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
