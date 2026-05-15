000040c82be0092657a379e1f0774afb2a843922
Kervy Tabigue <kervytabigue69@gmail.com>
2026-05-15 21:11:46 +0800

    Updated Files

--- frontend/src/app/_helpers/app.initializer.ts
+++ frontend/src/app/_helpers/app.initializer.ts
@@ -2,9 +2,10 @@ import { AccountService } from '@app/_services';
 
 export function appInitializer(accountService: AccountService) {
     return () => new Promise<void>(resolve => {
-        // Attempt to refresh token on app start up to auto authenticate
         accountService.refreshToken()
-            .subscribe()
-            .add(resolve);
-    }).catch(() => Promise.resolve());
+            .subscribe({
+                next: () => resolve(),
+                error: () => resolve()
+            });
+    });
 }
